c5f4e848546a57c432be387393d6a8cf3fe368f8
dciv <kkcf0123@gmail.com>
2024-04-11 17:51:48 +0900

    findby query  test

--- jpa/jpa/src/main/java/com/example/jpa/repository/BoardRepository.java
+++ jpa/jpa/src/main/java/com/example/jpa/repository/BoardRepository.java
@@ -1,9 +1,31 @@
 package com.example.jpa.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.jpa.entity.Board;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    List<Board> findByTitle(String title);
+
+    List<Board> findByTitleLike(String title);
+
+    List<Board> findByTitleStartingWith(String title);
+
+    List<Board> findByTitleEndingWith(String title);
+
+    List<Board> findByTitleContaining(String title);
+
+    List<Board> findByWriter(String writer);
+
+    List<Board> findByWriterStartingWith(String writer);
+
+    List<Board> findByTitleContainingOrContent(String title, String content);
+
+    List<Board> findByIdGreaterThanOrderByIdDesc(Long id);
+
+    List<Board> findByIdGreaterThanOrderByIdDesc(Long id, Pageable pageable);
 
 }
--- jpa/jpa/src/test/java/com/example/jpa/repository/BoardRepositoryTest.java
+++ jpa/jpa/src/test/java/com/example/jpa/repository/BoardRepositoryTest.java
@@ -1,11 +1,14 @@
 package com.example.jpa.repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.LongStream;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import com.example.jpa.entity.Board;
 
@@ -56,5 +59,31 @@ public class BoardRepositoryTest {
         Optional<Board> result = boardRepository.findById(24L);
         // Board board = result.get();
         // boardRepository.delete(board);
+    }
+
+    @Test
+    public void queryMethodTest() {
+        // List<Board> list = boardRepository.findByTitle("Title");
+        // System.out.println("findbytitle : " + list.size());
+        // list = boardRepository.findByTitleLike("Title");
+        // System.out.println("findbytitleLike : " + list.size());
+        // list = boardRepository.findByTitleStartingWith("Title");
+        // System.out.println("findbytitleStartingWith : " + list.size());
+        // list = boardRepository.findByTitleEndingWith("Title");
+        // System.out.println("findbytitleEndingWith : " + list.size());
+        // list.forEach(System.out::println);
+
+        // List<Board> list = boardRepository.findByWriterStartingWith("User");
+        // list.forEach(System.out::println);
+
+        // where b1_0.title like ? escape '\' or b1_0.content=?
+        // List<Board> list = boardRepository.findByTitleContainingOrContent("Title",
+        // "Content");
+        // list.forEach(System.out::println);
+
+        Pageable pageable = PageRequest.of(1, 10);
+        List<Board> list = boardRepository.findByIdGreaterThanOrderByIdDesc(0L, pageable);
+        list.forEach(System.out::println);
+        // System.out.println("Greater " + list.size());
     }
 }
